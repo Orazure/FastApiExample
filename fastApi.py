bf6e759324c809f2d81ec51d110ad2540b4340f4
ALEXA <thealactraz@gmail.com>
2022-11-25 11:12:39 +0100

    fix

--- fastApi.py
+++ fastApi.py
@@ -1,9 +1,7 @@
 from typing import Union
-
 from fastapi import FastAPI
 from pydantic import BaseModel
-# import json
-import json, requests
+import json, requests, uvicorn
 from fastapi.responses import JSONResponse
 
 
@@ -21,17 +19,16 @@ PATH = "https://www.georisques.gouv.fr/"
 @app.get("/get_datas_AZI")
 async def get_data(code_insee: str, range: int = 100, page : int = 1,page_size : int = 10):
     """
-    --AZI--
-     def :Opérations sur les Atlas des Zones Inondables (AZI)
+    AZI
+        def :Opérations sur les Atlas des Zones Inondables (AZI)
     get the data from the API
-
+    
     args:
         code_insee (str): the code of the city
         range (str): the range of the data
         page (int): the page of the data
         page_size (int): the size of the page
     """
-    # get the data from the API
     url = PATH + "api/v1/gaspar/catnat"
     if(len(code_insee) != 5):
         return JSONResponse(status_code=404, content={"message": "code_insee is not valid"})
@@ -45,13 +42,11 @@ async def get_data(code_insee: str, range: int = 100, page : int = 1,page_size :
     data=response.json()
     return data.get("data")
     
-    
-
 @app.get("/get_datas_catnat")
 async def get_datas_catnat(code_insee: str, range: int = 100, page : int = 1,page_size : int = 10):
     """
-    --catnat--
-    def :Opérations sur les données de catastrophes naturelles
+    catnat
+        def :Opérations sur les données de catastrophes naturelles
     args:
         code_insee (str): the code of the city
         range (str): the range of the data
@@ -59,7 +54,15 @@ async def get_datas_catnat(code_insee: str, range: int = 100, page : int = 1,pag
         page_size (int): the size of the page
     """
     url = PATH + "api/v1/gaspar/catnat"
-    response = requests.get(url)
+    if(len(code_insee) != 5):
+        return JSONResponse(status_code=404, content={"message": "code_insee is not valid"})
+    params = {
+        "code_insee": code_insee,
+        "range": range,
+        "page": page,
+        "page_size": page_size
+    }
+    response = requests.get(url, params=params)
     data=response.json()
     return data.get("data")
 
@@ -67,6 +70,4 @@ async def get_datas_catnat(code_insee: str, range: int = 100, page : int = 1,pag
 
 
 if __name__ == "__main__":
-    import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=8000)
